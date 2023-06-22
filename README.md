d7535423c66fa19899622577099a96baea2d160b
Sanjeev Easparan <easparansanjeev@gmail.com>
2023-06-22 10:59:38 -0400

    Added basic Chat UI

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chatbot-ui
 a flutter application serving as the chatbot UI
+
+## Installing Flutter
+
+Please follow instructions on https://docs.flutter.dev/get-started/install and make sure to select the OS you're currently using
+
+Make sure your preferred editor is also set up to use Flutter.
+
+(For example if using VSCode, install the Flutter extension)
---
+++ chatbot_ui/lib/messages.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MessagesScreen extends StatefulWidget {
+  final List messages;
+  const MessagesScreen({Key? key, required this.messages}) : super(key: key);
+
+  @override
+  _MessagesScreenState createState() => _MessagesScreenState();
+}
+
+class _MessagesScreenState extends State<MessagesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var w = MediaQuery.of(context).size.width;
+    return ListView.separated(
+        itemBuilder: (context, index) {
+          return Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: widget.messages[index]['isUser']
+                  ? MainAxisAlignment.end
+                  : MainAxisAlignment.start,
+              children: [
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 14, horizontal: 14),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(widget.messages[index]['isUser'] ? 0 : 20),
+                          topLeft: Radius.circular(widget.messages[index]['isUser'] ? 20 : 0),
+                        ),
+                        color: widget.messages[index]['isUser']
+                            ? Colors.purple.shade200.withOpacity(0.8)
+                            : Colors.blue.shade200.withOpacity(0.8)),
+                    constraints: BoxConstraints(maxWidth: w * 2 / 3),
+                    child:
+                        Text(widget.messages[index]['msg'])),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (_, i) => Padding(padding: EdgeInsets.only(top: 10)),
+        itemCount: widget.messages.length);
+  }
+}
